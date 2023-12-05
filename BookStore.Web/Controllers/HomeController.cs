52c8ae8fa4708a77ebf213f4a15943c90d84a9ee
PremMahto-Developer <149678317+PremMahto-Developer@users.noreply.github.com>
2023-12-05 10:45:02 +0530

    controller committ

--- BookStore.Web/Controllers/HomeController.cs
+++ BookStore.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using BookStore.Data;
+using BookStore.Data.Models;
 using BookStore.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -33,9 +34,21 @@ namespace BookStore.Web.Controllers
 
         public IActionResult Cart()
         {
+            //userid 2, manually added in db& hardcoded for now
             var cartItems = _dbContext.CartItems.Where(x => x.UserId == 2).Include(i=>i.Book).ToList();
             return View(cartItems);
         }
+
+        public IActionResult Checkout()
+        {
+            //userid 2, manually added in db & hardcoded for now
+            var cartItems = _dbContext.CartItems.Where(x => x.UserId == 2).Include(i => i.Book).ToList();
+            decimal totalPrice = 0;
+            decimal quantity = 1;//hardcoded for now
+            totalPrice=cartItems.Sum(i => (i.Book.Price * quantity));
+            return View(totalPrice);
+        }
+
         public IActionResult Privacy()
         {
             return View();
